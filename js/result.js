e2b627f4dcfbc1f8f2c31572c664c5a5e0d04257
Sandeep Morya <laxmidassmorya@gmail.com>
2022-10-26 00:39:42 +0530

    minor change

--- js/result.js
+++ js/result.js
@@ -1,5 +1,12 @@
-import {navbar,search,up_angle,down_angle,id,keys} from "../modules/variables.js";
-import { homepage,search_data } from "../modules/methods.js";
+import {
+  navbar,
+  search,
+  up_angle,
+  down_angle,
+  id,
+  keys,
+} from "../modules/variables.js";
+import { homepage, search_data } from "../modules/methods.js";
 
 //--> Variable declairation
 
@@ -11,16 +18,18 @@ let active = false;
 let angle = document.querySelector(".angle");
 let list = document.querySelector(".list");
 let logo = document.querySelector(".logo");
-let output = document.querySelector('.output');
-let text_part=document.querySelector('.text_part');
-let p_img=document.querySelector('.p_img');
-let p_displayname=document.querySelector('.p_displayname');
-let p_username=document.querySelector('.p_username');
-let p_des=document.querySelector('.p_des');
-let p_url=document.querySelector('.p_url');
-let prev=document.querySelector('.prev');
-let next=document.querySelector('.next');
-let current=keys.lastIndexOf(id);
+let output = document.querySelector(".output");
+let text_part = document.querySelector(".text_part");
+let p_img = document.querySelector(".p_img");
+let p_displayname = document.querySelector(".p_displayname");
+let p_username = document.querySelector(".p_username");
+let p_des = document.querySelector(".p_des");
+let p_url = document.querySelector(".p_url");
+let prev = document.querySelector(".prev");
+let next = document.querySelector(".next");
+let current = keys.lastIndexOf(id);
+let search_bar = document.querySelector(".search_bar");
+let search_btn = document.querySelector(".search_btn");
 
 angle.innerHTML = down_angle;
 angle.addEventListener("click", () => {
@@ -35,20 +44,23 @@ angle.addEventListener("click", () => {
   }
 });
 logo.addEventListener("click", () => homepage());
+//---> search functionality
+search_btn.addEventListener("click", () => homepage());
+search_bar.addEventListener("click", (e) => homepage());
 
-  let url = `https://api.giphy.com/v1/gifs/${id}?api_key=EhZIr1LNpPT7Hy9tS0iDuSFsUMVkfwNj`;
+let url = `https://api.giphy.com/v1/gifs/${id}?api_key=EhZIr1LNpPT7Hy9tS0iDuSFsUMVkfwNj`;
 
-let respose=search_data(url);
-respose.then(e=>showResult(e))
+let respose = search_data(url);
+respose.then((e) => showResult(e));
 
-function showResult(e){
-  output.src=e.images.original.url;
-  p_img.src=e.user.avatar_url;
-  p_displayname.textContent=e.user.display_name;
-  p_username.textContent=e.user.username;
-  p_des.textContent=e.user.description;
-  p_url.textContent=e.user.website_url;
-  text_part.innerHTML=`${e.title}<i class="fa-solid fa-ellipsis"></i>`
+function showResult(e) {
+  output.src = e.images.original.url;
+  p_img.src = e.user.avatar_url;
+  p_displayname.textContent = e.user.display_name;
+  p_username.textContent = e.user.username;
+  p_des.textContent = e.user.description;
+  p_url.textContent = e.user.website_url;
+  text_part.innerHTML = `${e.title}<i class="fa-solid fa-ellipsis"></i>`;
 }
 //user.avatar_url
 //user.display_name
@@ -57,15 +69,19 @@ function showResult(e){
 //user.website_url
 
 //--> button clicks
-prev.addEventListener('click',()=>{
-  let url = `https://api.giphy.com/v1/gifs/${keys[--current]}?api_key=EhZIr1LNpPT7Hy9tS0iDuSFsUMVkfwNj`;
+prev.addEventListener("click", () => {
+  let url = `https://api.giphy.com/v1/gifs/${
+    keys[--current]
+  }?api_key=EhZIr1LNpPT7Hy9tS0iDuSFsUMVkfwNj`;
 
-  let respose=search_data(url);
-  respose.then(e=>showResult(e)).catch(e=>console.log('try next step'));
+  let respose = search_data(url);
+  respose.then((e) => showResult(e)).catch((e) => console.log("try next step"));
 });
-next.addEventListener('click',()=>{
-  let url = `https://api.giphy.com/v1/gifs/${keys[++current]}?api_key=EhZIr1LNpPT7Hy9tS0iDuSFsUMVkfwNj`;
+next.addEventListener("click", () => {
+  let url = `https://api.giphy.com/v1/gifs/${
+    keys[++current]
+  }?api_key=EhZIr1LNpPT7Hy9tS0iDuSFsUMVkfwNj`;
 
-  let respose=search_data(url);
-  respose.then(e=>showResult(e)).catch(e=>console.log('try next step'));
+  let respose = search_data(url);
+  respose.then((e) => showResult(e)).catch((e) => console.log("try next step"));
 });
